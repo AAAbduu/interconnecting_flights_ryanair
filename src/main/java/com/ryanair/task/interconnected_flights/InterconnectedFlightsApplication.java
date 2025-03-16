d3aaa71a9b89fd9df9360843db3873591decd559
abdu <anawabduali@gmail.com>
2025-03-16 13:04:54 +0100

    initial commit

---
+++ src/main/java/com/ryanair/task/interconnected_flights/InterconnectedFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.ryanair.task.interconnected_flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterconnectedFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterconnectedFlightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanair/task/interconnected_flights/InterconnectedFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanair.task.interconnected_flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterconnectedFlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
